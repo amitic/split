eaf7ef74c6b411b1b2721cfd2fd5baddb191c4f1
Aleksandar Mitic <amitic@gmail.com>
2017-04-09 07:51:37 +0200

    added encoding support

--- index.js
+++ index.js
@@ -5,59 +5,62 @@
 // the most basic reduce just emits one 'data' event after it has recieved 'end'
 
 
-var through = require('through')
-var Decoder = require('string_decoder').StringDecoder
+const through = require('through')
+const Decoder = require('string_decoder').StringDecoder
 
 module.exports = split
 
 //TODO pass in a function to map across the lines.
 
-function split (matcher, mapper, options) {
-  var decoder = new Decoder()
-  var soFar = ''
-  var maxLength = options && options.maxLength;
-  var trailing = options && options.trailing === false ? false : true
-  if('function' === typeof matcher)
+function split(matcher, mapper, options) {
+  let encoding = options && typeof options.encoding !== 'undefined' ? options.encoding : undefined
+  let decoder = new Decoder(encoding)
+  let soFar = ''
+  let maxLength = options && options.maxLength;
+  let trailing = options && options.trailing === false ? false : true
+  if ('function' === typeof matcher)
     mapper = matcher, matcher = null
   if (!matcher)
     matcher = /\r?\n/
 
-  function emit(stream, piece) {
-    if(mapper) {
-      try {
-        piece = mapper(piece)
-      }
-      catch (err) {
-        return stream.emit('error', err)
-      }
-      if('undefined' !== typeof piece)
-        stream.queue(piece)
-    }
-    else
-      stream.queue(piece)
+  function queue(stream, piece) {
+    stream.queue(Buffer.from(piece, encoding))
   }
 
-  function next (stream, buffer) {
-    var pieces = ((soFar != null ? soFar : '') + buffer).split(matcher)
+  function emit(stream, piece) {
+    if (mapper) {
+      try {
+        piece = mapper(piece)
+      } catch (err) {
+        return stream.emit('error', err)
+      }
+      if ('undefined' !== typeof piece)
+        queue(stream, piece)
+    } else
+      queue(stream, piece)
+  }
+
+  function next(stream, buffer) {
+    let pieces = ((soFar != null ? soFar : '') + buffer).split(matcher)
     soFar = pieces.pop()
 
     if (maxLength && soFar.length > maxLength)
       stream.emit('error', new Error('maximum buffer reached'))
 
-    for (var i = 0; i < pieces.length; i++) {
-      var piece = pieces[i]
+    for (let i = 0; i < pieces.length; i++) {
+      let piece = pieces[i]
       emit(stream, piece)
     }
   }
 
   return through(function (b) {
-    next(this, decoder.write(b))
-  },
-  function () {
-    if(decoder.end)
-      next(this, decoder.end())
-    if(trailing && soFar != null)
-      emit(this, soFar)
-    this.queue(null)
-  })
+      next(this, decoder.write(b))
+    },
+    function () {
+      if (decoder.end)
+        next(this, decoder.end())
+      if (trailing && soFar != null)
+        emit(this, soFar)
+      this.queue(null)
+    })
 }
